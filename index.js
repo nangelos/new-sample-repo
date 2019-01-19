0bfe500107e15ada0cca3ce958a362843383f57b
Matthew Murnighan <mattmurn@gmail.com>
2019-01-19 11:36:21 -0600

    added another add function call

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ console.log('This thing is working');
 let sum = 0;
 const add = (num1, num2) => {
   sum = num1 + num2;
-  console.log('matt was here');
+  console.log('matt was here again');
   console.log(sum);
   return sum;
 };
 
 add(3, 4);
+add(3, 10000000000);
